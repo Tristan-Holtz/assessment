94b2171ead7c835c7351ca069d9f4bfa8cef9239
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2020-03-04 11:58:33 -0700

    Complete iterations 1-5

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,16 @@
 export const getUrls = () => {
-  return fetch('http://localhost:3001/api/v1/urls')
-      .then(response => response.json())
-}
+  return fetch('http://localhost:3001/api/v1/urls').then(response =>
+    response.json()
+  );
+};
+
+export const postUrl = (url, title) => {
+  const options = {
+    method: 'POST',
+    body: JSON.stringify({ long_url: url, title: title }),
+    headers: { 'Content-Type': 'application/json' }
+  };
+  return fetch('http://localhost:3001/api/v1/urls', options).then(response =>
+    response.json()
+  );
+};
--- src/components/UrlContainer/UrlContainer.js
+++ src/components/UrlContainer/UrlContainer.js
@@ -1,22 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './UrlContainer.css';
+import { connect } from 'react-redux';
+import { setUrls } from '../../actions';
+import { getUrls } from '../../apiCalls';
 
-const UrlContainer = props => {
-  const urlEls = props.urls.map(url => {
+export class UrlContainer extends Component {
+  async componentDidMount() {
+    await getUrls()
+      .then(data => this.props.setUrls(data.urls))
+      .catch(err => console.error('Error fetching:', err));
+  }
+
+  render() {
+    const urlEls = this.props.urls.map(url => {
+      return (
+        <div className="url">
+          <h3>{url.title}</h3>
+          <a href={url.short_url} target="blank">
+            {url.short_url}
+          </a>
+          <p>{url.long_url}</p>
+        </div>
+      );
+    });
     return (
-      <div className="url">
-        <h3>{url.title}</h3>
-        <a href={url.short_url} target="blank">{url.short_url}</a>
-        <p>{url.long_url}</p>
-      </div>
-    )
-  });
-
-  return (
-    <section>
-      { urlEls.length ? urlEls : <p>No urls yet! Find some to shorten!</p> }
-    </section>
-  )
+      <section>
+        {urlEls.length ? urlEls : <p>No urls yet! Find some to shorten!</p>}
+      </section>
+    );
+  }
 }
 
-export default UrlContainer;
+export const mapDispatchToProps = dispatch => ({
+  setUrls: urls => {
+    dispatch(setUrls(urls));
+  }
+});
+
+export const mapStateToProps = state => ({
+  urls: state.urls
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(UrlContainer);
